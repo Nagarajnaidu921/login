db100ebcf74650ee5b302a646347ba1aad8dcf2b
Yuvaraj <shivarajnaidu@gmail.com>
2017-05-15 10:40:19 +0530

    Style Changes... Follow the style and conventions which i followed in this code sample

--- app/app.js
+++ app/app.js
@@ -1,55 +1,84 @@
-var app = angular.module("myapp",[]);
-app.controller("login",($scope , $http , $templateCache)=>{
-	$scope.login=()=>{
-		$http({
-			method:'POST',
-			url:'reg.php',
-			data:{email : $scope.email, password : $scope.password},
-			headers: {'Content-Type': 'application/x-www-form-urlencoded'},  
-            cache: $templateCache
-		}).then((res)=>{
-			if(res.data !="n")
-			{
-				$scope.successMsg=res.data;	
-			}
-			else if(res.data == "n" ){
-				$scope.errorMsg="username and password mismatch";
-			}
-		})
-	}
-});
+'use strict';
+
+(function() {
+    /* 
+     * Space Should Be Given After Function key Word..
+     * Should Write Like This function () {} 
+     */
+
+    var app = angular.module('myapp', []);
+
+    /*
+     * Login Controller
+     */
+
+    app.controller('login', function($scope, $http) {
+        $scope.login = function() {
+            var userData = {
+                email: $scope.email,
+                password: $scope.password
+            };
+
+            $http.post('reg.php', userData)
+                .then(function(res) {
+                    if (res.data != 'n') {
+                        $scope.successMsg = res.data;
+                    } else if (res.data == 'n') {
+                        $scope.errorMsg = 'username and password mismatch';
+                    }
+                })
+        }
+    });
 
 
+    /*
+     * SignUp Controller.. Controller
+     */
 
-app.controller("signup",function($scope , $http , $templateCache){
-	
-	$scope.Signup=()=>{
-		if(($scope.password==$scope.rpassword)){
-			if($scope.password.length>8){
-				$http({
-					method:'POST',
-					url:'signup.php',
-					data:{firstname:$scope.firstname,lastname:$scope.lastname , email:$scope.email , password:$scope.password},
-					headers:{'Content-Type': 'application/x-www-form-urlencoded'},
-					cache:$templateCache
-				}).then((res)=>{
-					if(res.data=="success")
-					{
-						window.location.href="index.html";
-					}else{
-						$scope.errorMsg="email id already associated with some other account";
-					}
-				})
-			}
-			else{
-				$scope.errorMsg="password should have atleast 8 characters";
-			}
-		}
-		else{
+    app.controller('signup', function($scope, $http) {
 
-			$scope.errorMsg="password field are not matching";
-		}
-	}
-	
-	
-});
+        $scope.signUp = function() {
+
+            /*
+             * Form Values..
+             */
+            var firstName = $scope.firstname;
+            var lastName = $scope.lastname;
+            var email = $scope.email;
+            var password = $scope.password;
+            var rPassword = $scope.rpassword;
+
+            /*
+             * Password Validation Requirements..
+             */
+            var isPasswordsSame = password === rPassword;
+            var isPassLengthOk = password.length >= 8;
+
+            if (isPasswordsSame && isPassLengthOk) {
+
+                var userData = {
+                	// Data Which We Are Going To Send To Server.. Which Will Process Data And Produce Output..
+                    firstname: firstName,
+                    lastname: lastName,
+                    email: email,
+                    password: password
+                };
+
+                $http.post('signup.php', userData)
+                    .then(function(res) {
+                        var data = res.data;
+                        if (data == 'success') {
+                            window.location.href = 'index.html';
+                        } else {
+                            $scope.errorMsg = 'email id already associated with some other account';
+                        }
+                    })
+            } else if (!isPasswordsSame) {
+                $scope.errorMsg = 'Passwords Should Be Same';
+            } else if (!isPassLengthOk) {
+                $scope.errorMsg = 'Minimum Password Length Should Be Atleast 8';
+            }
+        }
+
+    });
+})();
--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 $db_server="localhost";
 $db_user="root";
 $db_password="root";
-$db="mydb";
+$db="test";
 $conn = new mysqli($db_server,$db_user,$db_password,$db);
     if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
